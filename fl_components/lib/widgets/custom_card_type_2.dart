32f2b10577f99139eaec65759ea9b06053f6b9ca
eidepozo <eidepozo@gmail.com>
2022-01-13 09:53:38 -0300

    Agrega configuraciones tarjeta tipo 2

--- fl_components/lib/widgets/custom_card_type_2.dart
+++ fl_components/lib/widgets/custom_card_type_2.dart
@@ -1,3 +1,4 @@
+import 'package:fl_components/themes/app_themes.dart';
 import 'package:flutter/material.dart';
 
 class CustomCardType2 extends StatelessWidget {
@@ -6,16 +7,25 @@ class CustomCardType2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-        elevation: 10,
+        clipBehavior: Clip.antiAlias,
+        elevation: 30,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+        shadowColor: AppThemes.primary.withOpacity(0.5),
         child: Column(
-          children: const [
-            FadeInImage(
-                image: NetworkImage(
-                    'https://www.hola.com/imagenes/cine/2009121411875/taylorlautner/programa/humor/0-24-552/24552-a.jpg'),
-                placeholder: AssetImage('assets/jar-loading.gif')),
-            /*Image(
-                image: NetworkImage(
-                    'https://www.hola.com/imagenes/cine/2009121411875/taylorlautner/programa/humor/0-24-552/24552-a.jpg'))*/
+          children: [
+            const FadeInImage(
+              image: NetworkImage(
+                  'https://pbs.twimg.com/profile_images/1104968010094260224/nZviTxtE_400x400.jpg'),
+              placeholder: AssetImage('assets/jar-loading.gif'),
+              width: double.infinity,
+              height: 400,
+              fit: BoxFit.cover,
+              fadeInDuration: Duration(milliseconds: 300),
+            ),
+            Container(
+                alignment: AlignmentDirectional.center,
+                padding: const EdgeInsets.only(right: 20, top: 10, bottom: 10),
+                child: const Text('#MADPENGUIN'))
           ],
         ));
   }
