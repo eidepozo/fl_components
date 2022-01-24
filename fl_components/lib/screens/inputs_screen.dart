60146968dde15b9fd9d121facb7d5350740fc2ca
eidepozo <eidepozo@gmail.com>
2022-01-24 17:30:52 -0300

    Agrega personalizacion input

--- fl_components/lib/screens/inputs_screen.dart
+++ fl_components/lib/screens/inputs_screen.dart
@@ -20,6 +20,21 @@ class InputScreen extends StatelessWidget {
                       onChanged: (value) {
                         print('value: $value');
                       },
+                      validator: (value) {
+                        if (value == null) return 'Este campo es requerido';
+                        return value.length < 3 ? 'Minimo de 3 letras' : null;
+                      },
+                      autovalidateMode: AutovalidateMode.onUserInteraction,
+                      decoration: const InputDecoration(
+                          hintText: 'Nombre del usuario',
+                          labelText: 'Nombre',
+                          helperText: 'Solo letras',
+                          suffixIcon: Icon(Icons.verified_user_outlined),
+                          icon: Icon(Icons.keyboard_capslock_rounded),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.only(
+                                  bottomLeft: Radius.circular(10),
+                                  topRight: Radius.circular(10)))),
                     )
                   ],
                 ))));
