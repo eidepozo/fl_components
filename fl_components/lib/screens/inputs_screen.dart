84ad1550d99529a22fa07968db401c8748d41506
eidepozo <eidepozo@gmail.com>
2022-01-25 10:30:14 -0300

    Agrega primera logica para guardar contenido formulario

--- fl_components/lib/screens/inputs_screen.dart
+++ fl_components/lib/screens/inputs_screen.dart
@@ -6,33 +6,59 @@ class InputScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final myFormKey = GlobalKey<FormState>();
+
+    final Map<String, String> formValues = {
+      'first_name': 'Elliot',
+      'last_name': 'Ide',
+      'email': 'eidepozo@gmail.com',
+      'password': '123456',
+      'role': 'Admin'
+    };
     return Scaffold(
         appBar: AppBar(title: const Text('Input Screen')),
         body: SingleChildScrollView(
             child: Padding(
                 padding:
                     const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                child: Column(
-                  children: const [
-                    CustomInputField(
-                        hintText: 'Nombre del usuario', labelText: 'Nombre'),
-                    SizedBox(height: 20),
-                    CustomInputField(
-                        hintText: 'Apellido del usuario',
-                        labelText: 'Apellido'),
-                    SizedBox(height: 20),
-                    CustomInputField(
-                      hintText: 'Correo del usuario',
-                      labelText: 'Email',
-                      keyboardType: TextInputType.emailAddress,
-                    ),
-                    SizedBox(height: 20),
-                    CustomInputField(
-                      hintText: 'Clave del usuario',
-                      labelText: 'Contraseña',
-                      obscureText: true,
-                    ),
-                  ],
+                child: Form(
+                  key: myFormKey,
+                  child: Column(
+                    children: [
+                      const CustomInputField(
+                          hintText: 'Nombre del usuario', labelText: 'Nombre'),
+                      const SizedBox(height: 20),
+                      const CustomInputField(
+                          hintText: 'Apellido del usuario',
+                          labelText: 'Apellido'),
+                      const SizedBox(height: 20),
+                      const CustomInputField(
+                        hintText: 'Correo del usuario',
+                        labelText: 'Email',
+                        keyboardType: TextInputType.emailAddress,
+                      ),
+                      const SizedBox(height: 20),
+                      const CustomInputField(
+                        hintText: 'Clave del usuario',
+                        labelText: 'Contraseña',
+                        obscureText: true,
+                      ),
+                      const SizedBox(height: 20),
+                      ElevatedButton(
+                          onPressed: () {
+                            FocusScope.of(context).requestFocus(FocusNode());
+                            // dispara las validaciones de cada input
+                            if (!myFormKey.currentState!.validate()) {
+                              print('Formulario no válido');
+                              return;
+                            }
+                            print(formValues);
+                          },
+                          child: const SizedBox(
+                              width: double.infinity,
+                              child: Center(child: Text('Guardar'))))
+                    ],
+                  ),
                 ))));
   }
 }
